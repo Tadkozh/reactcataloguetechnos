ec6fdef6687269b0c3153dd279813adfc3e32e1e
Tadkozh <tad.kozh.bihan@free.fr>
2022-10-16 00:59:16 +0200

    Distinguer quel bouton du menu est actif (ie sur quelle page on est) + suppression des background color

--- src/App.js
+++ src/App.js
@@ -12,6 +12,7 @@ function App() {
       <Menu />
       <Routes>
         <Route path='/' element={<Home />} />
+        <Route path='/home' element={<Home />} />
         <Route path='/add' element={<TechnoAdd />} />
         <Route path='/list' element={<TechnoList />} />
       </Routes>
